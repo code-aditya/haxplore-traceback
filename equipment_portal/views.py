3d139d39abfea8ce4c08f5bbead769eb843be971
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2019-08-25 02:35:17 +0530

    Completed Euipment Upload

--- equipment_portal/views.py
+++ equipment_portal/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from core.models import *
 from django.utils.timezone import now
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404,redirect,render
+from core import EQUIPMENT_TYPE_CHOICES,EQUIPMENT_TYPE_MAPS
+from .forms import EquipmentUploadForm
+from django.contrib import messages
 # Create your views here.
 
 def equipments_list(request):
@@ -12,7 +14,23 @@ def equipments_list(request):
     return render(request, 'equipment_list.html', context=context)
 
 def equipment_new(request):
-    return render(request, 'equipment_upload.html')
+    context={
+        'categories':EQUIPMENT_TYPE_CHOICES,
+    }
+    if request.method == "POST":
+        post_data = request.POST
+        data=dict(farmer=request.user.farmer.id, name=post_data['name'], rental_price=int(post_data['rental_price']),
+            start_dt=post_data['start_date'], end_dt=post_data['end_date'],category=int(post_data['category']),
+            description=post_data['description']
+        )
+        form = EquipmentUploadForm(data)
+        if form.is_valid():
+            form.save()
+            return redirect('equipments-list')
+        else:
+            print(form.errors)
+            messages.warning(request,"Form was not valid")
+    return render(request, 'equipment_upload.html', context=context)
 
 def equipment_detail(request, pk):
     template_name = 'equipment_detail.html'
