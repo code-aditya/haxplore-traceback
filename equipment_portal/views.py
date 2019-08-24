597a930b257f608cfe997e7384d054550777fd92
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2019-08-24 17:53:58 +0530

    Updated templates

--- equipment_portal/views.py
+++ equipment_portal/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def product_list(request):
-    template_name = 'product_list.html'
+    template_name = 'catalog.html'
     return render(request,template_name)
 
 def product_detail(request):
-    template_name = 'product_detail.html'
+    template_name = 'community_question.html'
     return render(request, template_name)
 
 def product_upload(request):
-    template_name = 'prodcut_upload.html'
+    template_name = 'login.html'
     return render(request,template_name)
